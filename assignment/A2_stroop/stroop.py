d5fe3656db25208827f4790fec1aab3bff84e4eb
khuyen-le <lenhakhuyen@gmail.com>
2025-02-15 15:38:24 -0800

    3. wait for response

--- assignment/A2_stroop/stroop.py
+++ assignment/A2_stroop/stroop.py
@@ -8,11 +8,14 @@ stimuli = ['red', 'orange', 'yellow', 'green', 'blue']
 
 win = visual.Window([800,600],color="gray", units='pix',checkTiming=False)
 placeholder = visual.Rect(win,width=180,height=80, fillColor="lightgray",lineColor="black", lineWidth=6,pos=[0,0])
+#maybe should also autoDraw placeholder? 
 word_stim = visual.TextStim(win,text="", height=40, color="black",pos=[0,0])
 instruction = visual.TextStim(win,text="Press the first letter of the ink color", height=20, color="black",pos=[0,-200])
 instruction.autoDraw = True
 
 fixation = visual.TextStim(win,text="+", height=15, color="black",pos=[0,0])
+
+response_keys = ['r', 'o', 'y', 'g', 'b', 'q'] #'q' to escape
 
 while True:
     # display fixation cross for 500ms
@@ -38,14 +41,16 @@ while True:
     instruction.draw()
     word_stim.draw()
     win.flip()
-    core.wait(1.0)
-
-    #wipe the stim
-    placeholder.draw()
-    instruction.draw()    
-    win.flip()
-    core.wait(.15)
-
-    if event.getKeys(['q']):
+    
+    #get the first key press
+    response = event.waitKeys(keyList=response_keys)[0] 
+    if response != 'q': 
+        #continue to next stim
+        placeholder.draw()
+        instruction.draw()    
+        win.flip()
+        core.wait(.15)
+    if response == 'q':
         win.close()
         core.quit()
+        
