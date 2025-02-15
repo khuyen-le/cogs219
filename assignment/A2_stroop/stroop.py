9171db3176f396e9b147081bccc858c89c3d0360
khuyen-le <lenhakhuyen@gmail.com>
2025-02-15 15:27:41 -0800

    1. fixation cross

--- assignment/A2_stroop/stroop.py
+++ assignment/A2_stroop/stroop.py
@@ -10,8 +10,27 @@ win = visual.Window([800,600],color="gray", units='pix',checkTiming=False)
 placeholder = visual.Rect(win,width=180,height=80, fillColor="lightgray",lineColor="black", lineWidth=6,pos=[0,0])
 word_stim = visual.TextStim(win,text="", height=40, color="black",pos=[0,0])
 instruction = visual.TextStim(win,text="Press the first letter of the ink color", height=20, color="black",pos=[0,-200])
+
+fixation = visual.TextStim(win,text="+", height=15, color="black",pos=[0,0])
+
 while True:
+    # display fixation cross for 500ms
+    placeholder.draw()
+    instruction.draw()
+    fixation.draw()
+    win.flip()
+    core.wait(0.5)
+
+    # wipe fixation cross
+    placeholder.draw()
+    instruction.draw()
+    win.flip()
+    core.wait(0.5)
+    
+    #choose randomly from stimuli list
     cur_stim = random.choice(stimuli)
+    
+    #display each stim for 1.0 sec
     word_stim.setText(cur_stim)
     word_stim.setColor(cur_stim)
     placeholder.draw()
@@ -19,6 +38,8 @@ while True:
     word_stim.draw()
     win.flip()
     core.wait(1.0)
+
+    #wipe the stim
     placeholder.draw()
     instruction.draw()    
     win.flip()
