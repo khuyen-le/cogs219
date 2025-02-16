19e678a82e4ed265b2dd9d7aed11338646f6a534
khuyen-le <lenhakhuyen@gmail.com>
2025-02-15 19:14:39 -0800

    12. write data

---
+++ assignment/A2_stroop/helper.py
@@ -0,0 +1,61 @@
+from psychopy import visual, event, core, gui 
+import os
+import glob
+
+def getRuntimeVariables(runtime_vars, order, exp_title='Stroop'):
+    dlg = gui.DlgFromDict(runtime_vars, order=order, title=exp_title)
+    #got this from assignment code
+    if dlg.OK:
+        return runtime_vars
+    else: 
+        print('User Cancelled')
+
+#read in trials
+def import_trials(trial_filename, col_names=None, separator=','):
+    trial_file = open(trial_filename, 'r')
+    if col_names is None:
+        # Assume the first row contains the column names
+        col_names = trial_file.readline().rstrip().split(separator)
+    trials_list = []
+    #loop through each line in trial_file
+    for cur_trial in trial_file:
+        cur_trial = cur_trial.rstrip().split(separator)
+        assert len(cur_trial) == len(col_names) # make sure the number of column names = number of columns
+        #create a dict of pairwise values in col_names and cur_trial
+        trial_dict = dict(zip(col_names, cur_trial))
+        trials_list.append(trial_dict)
+    return trials_list
+
+#open data file
+def create_data_file(subj_code, separator=','): 
+    # create a data folder if it doesn't already exist
+    try:
+        os.mkdir('data')
+    except FileExistsError:
+        print('Data directory exists; proceeding to open file')
+
+    #open file to write data to and store a header
+    data_file = open(f"data/{subj_code}_data.csv",'w')
+    header = separator.join(["subj_code", "seed", "word", 'color', 'trial_type', 'orientation',
+                             "trial_num", "response", "is_correct", "rt"])
+    data_file.write(header+'\n')
+    return data_file
+
+def write_to_file(fileHandle, trial, response, separator=',', sync=False, add_newline=True): 
+    #get information of current trial
+    trial_info = [trial[_] for _ in trial]
+    #add response information
+    trial_info.extend(response)
+    #stringify
+    trial_response = map(str, trial_info)
+    line = separator.join([str(i) for i in trial_response])
+    print(line)
+    if add_newline: 
+        line += '\n'
+    try:
+        fileHandle.write(line)
+    except:
+        print('file is not open for writing')
+    if sync: #set sync=False to NOT close the file after writing each line.
+        fileHandle.flush()
+        os.fsync(fileHandle)
--- assignment/A2_stroop/stroop.py
+++ assignment/A2_stroop/stroop.py
@@ -4,6 +4,7 @@ import os
 import random
 from psychopy import visual,event,core,gui
 from generate_trials import generate_trials
+from helper import import_trials, getRuntimeVariables, create_data_file, write_to_file
 
 stimuli = ['red', 'orange', 'yellow', 'green', 'blue']
 trial_types = ['congruent', 'incongruent']
@@ -27,51 +28,19 @@ responseTimer = core.Clock()
 # get runtime variables
 order = ['subj_code', 'seed', 'num_reps']
 runtime_vars_template = {'subj_code':'stroop_101', 'seed': 101, 'num_reps': 25}
-
-def getRuntimeVariables(runtime_vars, order, exp_title='Stroop'):
-    dlg = gui.DlgFromDict(runtime_vars, order=order, title=exp_title)
-    #got this from assignment code
-    if dlg.OK:
-        return runtime_vars
-    else: 
-        print('User Cancelled')
-
 runtime_vars = getRuntimeVariables(runtime_vars_template, order=order)
 
 #generate trials
 generate_trials(runtime_vars['subj_code'], runtime_vars['seed'], runtime_vars['num_reps'])
 
-#read in trials
-def import_trials(trial_filename, col_names=None, separator=','):
-    trial_file = open(trial_filename, 'r')
-    if col_names is None:
-        # Assume the first row contains the column names
-        col_names = trial_file.readline().rstrip().split(separator)
-    trials_list = []
-    #loop through each line in trial_file
-    for cur_trial in trial_file:
-        cur_trial = cur_trial.rstrip().split(separator)
-        assert len(cur_trial) == len(col_names) # make sure the number of column names = number of columns
-        #create a dict of pairwise values in col_names and cur_trial
-        trial_dict = dict(zip(col_names, cur_trial))
-        trials_list.append(trial_dict)
-    return trials_list
-
+#list of all trials
 trials_list = import_trials(f"trials/{runtime_vars['subj_code']}_trials.csv")
 
-def make_incongruent(cur_color): 
-    """Return a random color that is different from the color passed in. 
-    :Parameters:
-        cur_color : string
-            Current color of the stimuli
-    """
-    #list comprehension solution from assignment code!
-    #much better than my original solution, 
-    #which is to randomly choosing from the full stimuli list in a while loop and hope for the best...
-    incongruent_colors = [stim for stim in stimuli if stim != cur_color]
-    return random.choice(incongruent_colors)
+#file handler for incoming responses
+data_file = create_data_file(runtime_vars['subj_code'])
 
-for cur_trial in trials_list:
+for idx, cur_trial in enumerate(trials_list):
+    trial_num = idx + 1 # keep track of 1-index trial number
     # display fixation cross for 500ms
     placeholder.draw()
     instruction.draw()
@@ -96,6 +65,8 @@ for cur_trial in trials_list:
     word_stim.setColor(cur_color)
     if cur_orientation == "upside_down": 
         word_stim.setOri(180)
+    else: 
+        word_stim.setOri(0)
     placeholder.draw()
     instruction.draw()
     word_stim.draw()
@@ -111,21 +82,21 @@ for cur_trial in trials_list:
         feedback_slow.draw()
         win.flip()
         core.wait(1)
+        response = [trial_num, "NA", 0, "More than 2000"]
+        write_to_file(data_file, cur_trial, response)
         
     elif key_pressed[0][0] != 'q':  
         rt = key_pressed[0][1]*1000 # convert to ms
         RTs.append(rt)
+        is_correct = 1 if key_pressed[0][0] == cur_color[0] else 0
+        response = [trial_num, key_pressed[0][0], is_correct, rt]
+        write_to_file(data_file, cur_trial, response)
         #if wrong, give feedback
-        #cur_color instead of cur_word!
-        if key_pressed[0][0] != cur_color[0]: 
+        if is_correct == 0:
             feedback_incorrect.draw()
             win.flip()
             core.wait(1)
-        #continue to next stim
-        #placeholder.draw()
-        #instruction.draw()    
-        #win.flip()
-        #core.wait(.15)
+
     elif key_pressed[0][0] == 'q':
         break
 
